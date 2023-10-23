e6af90f04c8c7ff88638e50c91992149ecf3b3f7
Ashutosh <ashutosh.na.99@gmail.com>
2023-10-23 12:01:37 +0530

    Bux Fixes

--- main.py
+++ main.py
@@ -1,31 +1,25 @@
-from fastapi import FastAPI, Request, WebSocket
+from fastapi import FastAPI, WebSocket
 # from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from starlette.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 import uuid
 import os
 import base64
 import json
 import httpx
 import uvicorn
-from fastapi.templating import Jinja2Templates
-from fastapi.middleware.cors import CORSMiddleware
 
-# from configs.prepare_env import download_all_files
-# from similarity_finder.get_similar_images import get_similar_images
+from configs.prepare_env import download_all_files
+from similarity_finder.get_similar_images import get_similar_images
 
 FACEMORPH_API_URL = "https://api.facemorph.me/api"
-FACEMORPH_ENCODE_IMAGE = "/encodeimage"
-FACEMORPH_GENERATE_IMAGE = "/face"
+FACEMORPH_ENCODE_IMAGE = "/encodeimage/"
+FACEMORPH_GENERATE_IMAGE = "/face/"
 
 EXPERIMENT_DIR = "experiments/"
 
 app = FastAPI()
-app.mount("/assets", StaticFiles(directory="assets", html=True), name="assets")
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
-app.mount("/experiments", StaticFiles(directory="experiments",
-          html=True), name="experiments")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -33,6 +27,12 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.mount("/assets", StaticFiles(directory="assets", html=True), name="assets")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+app.mount("/experiments", StaticFiles(directory="experiments",
+          html=True), name="experiments")
 
 test_html = """
 <!DOCTYPE html>
@@ -74,21 +74,18 @@ test_html = """
 </html>
 """
 
-
-def prepare_env():
-    return 0
-
-
 async def img_format(image, uuid):
     data = {'usrimg': open(image, 'rb')}
     j = {'tryalign': True}
 
-    async with httpx.AsyncClient(follow_redirects=True) as client:
+    async with httpx.AsyncClient() as client:
         r = await client.post(FACEMORPH_API_URL +
                               FACEMORPH_ENCODE_IMAGE, files=data, data=j)
-        rjson = r.text
-        values = {'guid': json.loads(rjson)['guid']}
-
+        
+        rjson = json.loads(r.text)
+        values = {'guid': rjson['guid']}
+        print(values)
+    
         r = await client.get(FACEMORPH_API_URL + FACEMORPH_GENERATE_IMAGE, params=values)
         rawimg = r.content
         with open(uuid + "/preprocessed_uploaded_image.jpeg", 'wb') as out_file:
@@ -98,8 +95,8 @@ async def img_format(image, uuid):
 
 
 @app.get("/")
-async def get(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+async def get():
+    return FileResponse("index.html")
 
 
 @app.websocket("/ws")
@@ -117,26 +114,39 @@ async def websocket_endpoint(websocket: WebSocket):
 
         # Process uploaded image
         uploaded_image = open(exp_dir + "/uploaded_image.jpeg", "wb")
+        tmp = data
         data = data.replace('data:image/jpeg;base64,', '')
         uploaded_image.write(base64.b64decode(data))
         uploaded_image.close()
-        await websocket.send_json({"status_code": 2, "exp_uuid": exp_uuid})
+        await websocket.send_json({"status_code": 2, "exp_uuid": exp_uuid, "image": tmp})
 
+        # preprocess: facemorph
         await img_format(exp_dir + "/uploaded_image.jpeg", exp_dir)
-        await websocket.send_json({"status_code": 3, "exp_uuid": exp_uuid})
+        preprocessed_file = open(exp_dir + "/preprocessed_uploaded_image.jpeg", "rb").read()
+        base64_utf8_str = base64.b64encode(preprocessed_file).decode('utf-8')
+        dataurl = f'data:image/jpeg;base64,{base64_utf8_str}'
+        await websocket.send_json({"status_code": 3, "exp_uuid": exp_uuid, "image": dataurl})
 
-        # similar_images = get_similar_images(t, exp_dir + "/uploaded_image.jpeg")
-        # await websocket.send_json({"status_code": 4, "exp_uuid": exp_uuid, images: similar_images})
+        # get similar faces
+        similar_images = get_similar_images(exp_dir + "/preprocessed_uploaded_image.jpeg")
+        similar_images.pop(0)
+        similar_images.pop()
+        similar_images_encoded = []
+        for image in similar_images:
+            similar_image_data = open("dataset/CACD2000/" + image, "rb").read()
+            base64_utf8_str = base64.b64encode(similar_image_data).decode('utf-8')
+            dataurl = f'data:image/jpeg;base64,{base64_utf8_str}'
+            similar_images_encoded.append({"imagename": image, "encodedstring": dataurl})      
+        await websocket.send_json({"status_code": 4, "exp_uuid": exp_uuid, "images": similar_images_encoded})
 
-        await websocket.send_json({"status_code": 5, "exp_uuid": exp_uuid, images: []})
+        # run sam
+        #run()
+        await websocket.send_json({"status_code": 5, "exp_uuid": exp_uuid, "images": []})
 
         # Completed
         await websocket.send_json({"status_code": 6, "exp_uuid": exp_uuid})
-
+        
 
 if __name__ == '__main__':
-    # download_all_files()
-
-    # global t.load("preprocessed_files/CACD2000_refined_images_embeddings_clusters.ann")
-
+    download_all_files()
     uvicorn.run("main:app")
